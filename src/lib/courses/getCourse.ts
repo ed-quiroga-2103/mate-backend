9ad81c1f5ca4b5e20dd89ea0e3d0175b5c33a69a
Eduardo <ed.quiroga.2103@gmail.com>
2022-02-20 20:43:03 -0600

    Updated questions service

--- src/lib/courses/getCourse.ts
+++ src/lib/courses/getCourse.ts
@@ -1,9 +1,18 @@
 import { client } from '../prisma';
 
 const getCourse = async (id) => {
-    const course = await client.course.findUnique(id).catch((error) => {
-        throw error;
-    });
+    const course = await client.course
+        .findUnique({
+            where: {
+                id,
+            },
+            include: {
+                graph: true,
+            },
+        })
+        .catch((error) => {
+            throw error;
+        });
 
     return course;
 };
